e9dd4749a04faea1ced9ec39e7ffb5da8aaa9092
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-02 00:18:46 +0530

    [swapnaneel] Add : Implemented NullPointer

---
+++ Programming Elements Practice/String/Level1/NullPointer.java
@@ -0,0 +1,13 @@
+package BridgeLabz.Strings;
+
+public class NullPointer {
+    public static void main(String[] args) {
+        String s=null;
+
+        try{
+            System.out.println(s.length());
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+}
