dcbabb4cb7de506210cfb550a381909b2277032d
Swapnaneel-05 <swapnaneelbhadra2005@gmail.com>
2025-09-04 17:06:32 +0530

    Delete 03-09-2025/BookMain.java

--- 03-09-2025/BookMain.java
+++
@@ -1,8 +0,0 @@
-package BridgeLabz.OOPSJava;
-
-public class BookMain {
-    public static void main(String[] args) {
-        Book b1=new Book("rahul","ABC",10);
-        b1.display();
-    }   
-}
