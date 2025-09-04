cb8cf8271b32cc7965e55dd85086862af5861d7f
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-04 17:04:52 +0530

    [Swapnanel] Add : Book Details

---
+++ 03-09-2025/Level1/BookMain.java
@@ -0,0 +1,8 @@
+package BridgeLabz.OOPSJava;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book b1=new Book("rahul","ABC",10);
+        b1.display();
+    }   
+}
