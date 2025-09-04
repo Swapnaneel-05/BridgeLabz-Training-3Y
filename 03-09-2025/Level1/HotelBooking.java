cd9602e15708bce83526b23ad23d470c302a2465
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-05 01:21:06 +0530

    [Swapnaneel] Add : Booking a room in Hotel

---
+++ 03-09-2025/Level1/HotelBooking.java
@@ -0,0 +1,30 @@
+package BridgeLabz.OOPSJava;
+
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+
+    HotelBooking() {
+        this.guestName = "Guest";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    void display() {
+        System.out.println("Guest: " + guestName + ", Room: " + roomType + ", Nights: " + nights);
+    }
+}
