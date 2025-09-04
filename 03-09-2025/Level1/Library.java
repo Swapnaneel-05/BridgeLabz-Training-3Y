fa78f6971b5e0137016c2dd1c07ba04c5bf29d4a
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-05 01:33:47 +0530

    [Swapnaneel] Add : Renting a Book From Library

---
+++ 03-09-2025/Level1/Library.java
@@ -0,0 +1,28 @@
+package BridgeLabz.OOPSJava;
+
+public class Library {
+    String title;
+    String author;
+    int price;
+    boolean available;
+
+    Library(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.available = true;
+    }
+
+    void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println(title + " borrowed successfully.");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+
+    void display() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Price: " + price + ", Available: " + available);
+    }
+}
---
+++ 03-09-2025/Level1/LibraryMain.java
@@ -0,0 +1,9 @@
+package BridgeLabz.OOPSJava;
+
+public class LibraryMain {
+    public static void main(String[] args) {
+        Library book1= new Library("Happy Potter", "JK Rowlings", 450);
+
+        book1.display();
+    }
+}
