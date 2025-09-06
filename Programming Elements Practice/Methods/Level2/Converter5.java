ebc230bc34bee9d14182db7a90816d2ec3bfd67d
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 22:02:40 +0530

    [Swapnaneel] Add: Converted Values

---
+++ Programming Elements Practice/Methods/Level2/Converter5.java
@@ -0,0 +1,31 @@
+package BridgeLabz.Method.Level2;
+
+public class Converter5 {
+    public static double convertYardsToFeet(double yards) {
+        return yards * 3;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        return feet * 0.333333;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        return meters * 39.3701;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        return inches * 0.0254;
+    }
+
+    public static double convertInchesToCentimeters(double inches) {
+        return inches * 2.54;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("5 yards = " + convertYardsToFeet(5) + " feet");
+        System.out.println("12 feet = " + convertFeetToYards(12) + " yards");
+        System.out.println("2 meters = " + convertMetersToInches(2) + " inches");
+        System.out.println("10 inches = " + convertInchesToMeters(10) + " meters");
+        System.out.println("7 inches = " + convertInchesToCentimeters(7) + " cm");
+    }
+}
