84b8580175e63298ed301a738eb12387fcfb158e
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 21:50:20 +0530

    [Swapnaneel] Add: Sum of Numbers

---
+++ Programming Elements Practice/Methods/Level1/NumberSum.java
@@ -0,0 +1,22 @@
+package BridgeLabz.Method.Level1;
+
+import java.util.Scanner;
+
+public class NumberSum {
+    public static int findSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int sum = findSum(n);
+        System.out.println("Sum of first " + n + " natural numbers: " + sum);
+        sc.close();
+    }
+}
