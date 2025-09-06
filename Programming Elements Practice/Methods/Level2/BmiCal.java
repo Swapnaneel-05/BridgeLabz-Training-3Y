96e95c05aaef815360756247b4cf9ae89e96e644
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 22:01:32 +0530

    [Swapnaneel] Add: Calculated BMI

---
+++ Programming Elements Practice/Methods/Level2/BmiCal.java
@@ -0,0 +1,42 @@
+package BridgeLabz.Method.Level2;
+
+import java.util.Scanner;
+
+public class BmiCal {
+    public static double calculateBMI(double weight, double heightCm) {
+        double heightM = heightCm / 100.0;
+        return weight / (heightM * heightM);
+    }
+
+    public static String getBMIStatus(double bmi) {
+        if (bmi < 18.5) return "Underweight";
+        else if (bmi < 24.9) return "Normal weight";
+        else if (bmi < 29.9) return "Overweight";
+        else return "Obese";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] data = new double[10][3]; // weight, height(cm), BMI
+        String[] status = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter weight (kg) of person " + (i + 1) + ": ");
+            data[i][0] = sc.nextDouble();
+            System.out.print("Enter height (cm) of person " + (i + 1) + ": ");
+            data[i][1] = sc.nextDouble();
+            data[i][2] = calculateBMI(data[i][0], data[i][1]);
+            status[i] = getBMIStatus(data[i][2]);
+        }
+
+        System.out.println("\n--- BMI Report ---");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Person " + (i + 1) +
+                    " | Weight: " + data[i][0] + " kg" +
+                    " | Height: " + data[i][1] + " cm" +
+                    " | BMI: " + String.format("%.2f", data[i][2]) +
+                    " | Status: " + status[i]);
+        }
+        sc.close();
+    }
+}
