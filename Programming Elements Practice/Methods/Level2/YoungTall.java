e6ccc2a7fd6da3fbcab168035614933dff337d46
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 22:08:41 +0530

    [Swapnaneel] Add: Youngest Tallest

---
+++ Programming Elements Practice/Methods/Level2/YoungTall.java
@@ -0,0 +1,43 @@
+package BridgeLabz.Method.Level2;
+
+import java.util.Scanner;
+
+public class YoungTall {
+    public static String findYoungest(int[] ages, String[] names) {
+        int minIndex = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return names[minIndex];
+    }
+
+    public static String findTallest(double[] heights, String[] names) {
+        int maxIndex = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return names[maxIndex];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height (in cm) of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        System.out.println("Youngest friend is: " + findYoungest(ages, names));
+        System.out.println("Tallest friend is: " + findTallest(heights, names));
+        sc.close();
+    }
+}
