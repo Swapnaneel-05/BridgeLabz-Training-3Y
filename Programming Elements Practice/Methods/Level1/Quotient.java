4a4fa252a14e7d68cccdb697ca59e16cb3a27d4b
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 21:53:53 +0530

    [Swapnaneel] Add: Reminder and Quotient

---
+++ Programming Elements Practice/Methods/Level1/Quotient.java
@@ -0,0 +1,24 @@
+package BridgeLabz.Method.Level1;
+
+import java.util.Scanner;
+
+public class Quotient {
+     public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int quotient = number / divisor;
+        int remainder = number % divisor;
+        return new int[]{remainder, quotient};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(number, divisor);
+        System.out.println("Quotient: " + result[1]);
+        System.out.println("Remainder: " + result[0]);
+        sc.close();
+    }
+}
