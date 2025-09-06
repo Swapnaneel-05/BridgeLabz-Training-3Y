399db38be52efc73e770c13822c2e01ab949e04d
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2025-09-06 21:56:48 +0530

    [Swapnaneel] Add: Number of handshake

---
+++ Programming Elements Practice/Methods/Level1/Handshake.java
@@ -0,0 +1,17 @@
+package BridgeLabz.Method.Level1;
+
+import java.util.Scanner;
+
+public class Handshake {
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfStudents = sc.nextInt();
+        int handshakes = calculateHandshakes(numberOfStudents);
+        System.out.println("Maximum number of handshakes: " + handshakes);
+        sc.close();
+    }
+}
